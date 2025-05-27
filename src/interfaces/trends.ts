0120945e45fea843f8e4d405d752c11097c6715d
BekuhAN <Bekuh988@rambler.ru>
2025-05-27 16:23:18 +0300

    v0.2

--- src/interfaces/trends.ts
+++
@@ -1,9 +0,0 @@
-import { type BaseModel } from "./base-model";
-import type { CatalogItemType } from "./catalog-item";
-
-export interface TrendsItem extends BaseModel<string>  {
-    title: string,
-    image: string,
-    description: string,
-    catalog?: CatalogItemType[]
-  }
